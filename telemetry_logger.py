f50e9a7257d2db817a45097471d1d705bb8ad60b
Jayaprakash8887 <Jayaprakash.narayanaswamy@tarento.com>
2023-12-22 17:46:56 +0530

    Issue #0000 bug: Kathaasagara telemetry logging

--- telemetry_logger.py
+++ telemetry_logger.py
@@ -1,3 +1,4 @@
+import json
 from fastapi import Request
 import requests
 import time
@@ -5,8 +6,9 @@ import os
 import uuid
 from logger import logger
 
-telemetryURL = os.environ["TELEMETRY_ENDPOINT_URL"]
+telemetryURL = os.environ.get("TELEMETRY_ENDPOINT_URL", "")
 ENV_NAME = os.environ.get("SERVICE_ENVIRONMENT","dev")
+TELEMETRY_LOG_ENABLED = os.environ.get("TELEMETRY_LOG_ENABLED", "true").lower() == "true"
 
 class TelemetryLogger:
     """
@@ -24,12 +26,13 @@ class TelemetryLogger:
 
         **kwargs:** Keyword arguments containing the event data.
         """
-        # Check for required fields
-        # if not ("eid" in event or "object" in event):
-        #     raise ValueError("Missing required field(s) for event: 'eid' or 'object'")
+        
         logger.info(f"Telemetry event: {event}")
-        self.events.append(event)
-
+        
+        if not TELEMETRY_LOG_ENABLED:
+            return
+        
+        self.events.append(event)   
         # Send logs if exceeding threshold
         if len(self.events) >= self.threshold:
             self.send_logs()
@@ -38,86 +41,109 @@ class TelemetryLogger:
         """
         Sends the captured telemetry logs using the requests library.
         """
-        data = {
-            "id": "api.djp.telemetry",
-            "ver": "1.0",
-            "params": {"msgid": str(uuid.uuid4())},
-            "ets": int(time.time() * 1000),
-            "events": self.events,
-        }
-        headers = {"Content-Type": "application/json"}
-        response = requests.post(self.url + "/v1/telemetry", json=data, headers=headers)
-        logger.debug(f"Telemetry API request data: {data}")
-        if response.status_code != 200:
-            logger.error(f"Error sending telemetry log: {response.status_code} - {response.text}")
-        else:
+        try:
+            data = {
+                    "id": "api.djp.telemetry",
+                    "ver": "3.1",
+                    "params": {"msgid": str(uuid.uuid4())},
+                    "ets": int(time.time() * 1000),
+                    "events": self.events
+            }
+            headers = {"Content-Type": "application/json"}
+            response = requests.post(self.url + "/v1/telemetry", json=data, headers=headers)
+            response.raise_for_status()
+            logger.debug(f"Telemetry API request data: {data}")
             logger.info("Telemetry logs sent successfully!")
+            # Reset captured events after sending
+            self.events = []
+        except requests.exceptions.RequestException as e:
+            logger.error(f"Error sending telemetry log: {e}", exc_info=True)    
 
-        # Reset captured events after sending
-        self.events = []
-
-    def prepare_log_event(self, request: Request, body, etype = "api_access", elevel = "INFO", message=""):
+    def prepare_log_event(self, eventInput: dict, etype="api_access", elevel="INFO", message=""):
         """
         Prepare a telemetry event dictionary with the specified values. 
         Args:
-            eid: Event identifier (e.g., "LOG").
+            eventInput: Event Input.
+            etype: Event type (default: "api_access").
+            elevel: Event level (default: "INFO").
             message: Event message.
-            user_id: User ID.
-            channel: Event channel (default: "01269878797503692810").
-            level: Event level (default: "ERROR").
-            pageid: Page ID where the event occurred (default: "/").
 
         Returns:
             A dictionary representing the telemetry event data.
         """
-
         data = {
             "eid": "LOG",
             "ets": int(time.time() * 1000),  # Current timestamp
-            "ver": "1.0",  # Version
+            "ver": "3.1",  # Version
             "mid": f"LOG:{round(time.time())}",  # Unique message ID
             "actor": {
                 "id": "sakhi-api-service",
                 "type": "System",
             },
             "context": {
-                "channel": request.headers.get("X-Source", ""),
+                "channel": "ejp",
                  "pdata": {
-                    "id": "djp.sakhi.api.service",
-                    "ver": "1.0.0",
+                    "id": "ejp.sakhi.api.service",
+                    "ver": "1.0",
                     "pid": ""
                 },
-                "env": ENV_NAME,
-                "sid": request.headers.get("x-request-id", ""), # Optional. session id of the requestor stamped by portal
-                "did": request.headers.get("x-device-id", ""), # Optional. uuid of the device, created during app installation
-                "cdata": [
-                    {
-                        "id": request.headers.get("x-consumer-id", ""),
-                        "type": "UserSession"
-                    },
-                    {
-                        "id": request.headers.get("X-Source", ""),
-                        "type": "Device"
-                    }
-                ]
+                "env": ENV_NAME
             },
-            "object": {}, 
-            "tags": [],
             "edata": {
                 "type": etype,
                 "level": elevel,
-                "message": message,
-                "params": [
-                    {
-                        "request":  body
-                    },
-                    {
-                        "method": str(request.method)
-                    },
-                    {
-                        "url": str(request.url)
-                    }
-                ]
+                "message": str(message).replace("'", "")
             }
         }
+
+        if eventInput.get("x-request-id", None):
+            data["context"]["sid"] = eventInput.get("x-request-id")
+
+        if eventInput.get("x-device-id", None):
+            data["context"]["did"] = eventInput.get("x-device-id")
+
+        eventCData = self.__getEventCData(eventInput)
+        if eventCData:
+            data["context"]["cdata"] = eventCData
+
+        eventEDataParams = self.__getEventEDataParams(eventInput)
+        if eventEDataParams:
+            data["edata"]["params"] = eventEDataParams
         return data
+
+    def __getEventCData(self, eventInput: dict):
+        eventCData = []
+        if eventInput.get("x-consumer-id", None) and eventInput.get("x-source", None):
+            eventCData = [
+                {
+                    "id": eventInput.get("x-consumer-id"),
+                    "type": "ConsumerId"
+                },
+                {
+                    "id": eventInput.get("x-source"),
+                    "type": "Source"
+                }
+            ]
+        return eventCData
+
+    def __getEventEDataParams(self, eventInput: dict):
+        eventEDataParams = [
+            {"method": str(eventInput.get("method"))},
+            {"url": str(eventInput.get("url"))},
+            {"status": eventInput.get("status_code")},
+            {"duration": int(eventInput.get("duration"))}
+        ]
+        flattened_dict = self.__flatten_dict(eventInput.get("body", {}))
+        for item in flattened_dict.items():
+            eventEDataParams.append({item[0]: item[1]})
+        return eventEDataParams
+
+    def __flatten_dict(self, d, parent_key='', sep='_'):
+        flattened = {}
+        for k, v in d.items():
+            new_key = f"{parent_key}{sep}{k}" if parent_key else k
+            if isinstance(v, dict):
+                flattened.update(self.__flatten_dict(v, new_key, sep=sep))
+            else:
+                flattened[new_key] = v
+        return flattened
