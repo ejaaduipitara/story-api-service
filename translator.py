769d8b7dfb12f6f6c8fd724aa065c0e85ac08f0d
Devaj Kumar <devaj.kumar@trigyn.com>
2025-03-27 19:19:38 +0530

    assam added

--- translator.py
+++ translator.py
@@ -12,6 +12,7 @@ from utils import *
 telemetryLogger = TelemetryLogger()
 
 asr_mapping = {
+    "as": "ai4bharat/conformer-multilingual-indo_aryan-gpu--t4"
     "bn": "ai4bharat/conformer-multilingual-indo_aryan-gpu--t4",
     "en": "ai4bharat/whisper-medium-en--gpu--t4",
     "gu": "ai4bharat/conformer-multilingual-indo_aryan-gpu--t4",
